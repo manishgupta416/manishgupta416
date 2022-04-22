94134fddaef4a01e44ea10ce2a0afc8c63cbbc5e
Manish Gupta <manish.info2020@gmail.com>
2022-04-22 11:34:37 +0530

    initial

---
+++ index.js
@@ -0,0 +1,72 @@
+// console.log("Hello");
+
+// //Values and Variables In Js   
+
+// var myVariableName = 'Manish';
+// console.log(myVariableName)
+
+// var myAge = 45  //case sensitive
+// var Myage=56
+// console.log(Myage) 
+
+// var var =35
+
+//Data Types
+
+// var myName = "Manish"
+// var age = 70
+// var imManish = true
+// // console.log(myName)
+// console.log(typeof(myName))
+// console.log(typeof(age));
+// console.log(typeof(imManish))
+
+// console.log(10 + "20")  // concate ho rha h
+// console.log(9- "5") //Bug
+// console.log("java" + "script")
+// console.log("manish" - "gupta"); //NaN
+
+// 1 -> true
+// 0 -> false
+
+
+//null vs undefined
+
+// var imUseless = null
+// console.log(imUseless)  //null
+// console.log(typeof(imUseless)) // return object --> 2nd bug
+
+// var imUseless2 
+// console.log(imUseless2) //undefined
+
+//NaN
+
+// var name1 = 'manish'
+// var age = 45
+
+// console.log(isNaN(name1))
+// console.log(isNaN(age))
+
+
+
+
+
+
+
+//DOM VS BOM(Browser Object Model)
+
+
+//Expression and operators
+
+// console.log(5+20)
+
+// var x =5
+// var y = 5
+
+// console.log(x==y) // true
+// console.log("Is both the x and y are equal or not " + x=y);
+
+
+
+
+ 
